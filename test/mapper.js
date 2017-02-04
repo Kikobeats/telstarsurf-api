35cd6825412f6971ccf52bc0aef24e58d97804b9
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-02-04 13:13:49 +0100

    Handle case when item is not present

--- test/mapper.js
+++ test/mapper.js
@@ -16,29 +16,27 @@ function getFixture () {
   }]
 }
 
+function getFixtureWithNoSize () {
+  return [{
+    name: 'E-TYPE',
+    brand: 'NORTHSAILS',
+    link: 'http://www.telstarsurf.com/windsurf/windsurf-sails/used-sails/54235/northsails-e-type/',
+    image: 'http://www.telstarsurf.com/cache/img/788690b3da56/500/500/max/max/e-type.jpeg',
+    rawPrice: '€ 499,95',
+    oldPrice: '€ 499,95',
+    newPrice: '€ 399,95',
+    sizes: null
+  }]
+}
+
 describe('telstarsurf-api » mapper', function () {
-  it('.getItem', function () {
-    const fixture = getFixture()[0]
-    const size = fixture.sizes[0]
-    const item = getItem(fixture, size)
+  describe('.getItem', function () {
+    it('get item based on a size', function () {
+      const fixture = getFixture()[0]
+      const size = fixture.sizes[0]
+      const item = getItem(fixture, size)
 
-    should(item).be.eql({
-      name: 'E-TYPE',
-      title: 'NORTHSAILS E-TYPE 7.3 €399',
-      brand: 'NORTHSAILS',
-      link: 'http://www.telstarsurf.com/windsurf/windsurf-sails/used-sails/54235/northsails-e-type/',
-      image: 'http://www.telstarsurf.com/cache/img/788690b3da56/500/500/max/max/e-type.jpeg',
-      price: 399,
-      size: '7.3'
-    })
-  })
-
-  it('.getItems', function () {
-    const fixture = getFixture()
-    const items = getItems(fixture)
-
-    should(items).be.eql([
-      {
+      should(item).be.eql({
         name: 'E-TYPE',
         title: 'NORTHSAILS E-TYPE 7.3 €399',
         brand: 'NORTHSAILS',
@@ -46,16 +44,61 @@ describe('telstarsurf-api » mapper', function () {
         image: 'http://www.telstarsurf.com/cache/img/788690b3da56/500/500/max/max/e-type.jpeg',
         price: 399,
         size: '7.3'
-      },
-      {
-        name: 'E-TYPE',
-        title: 'NORTHSAILS E-TYPE 8.2 €399',
-        brand: 'NORTHSAILS',
-        link: 'http://www.telstarsurf.com/windsurf/windsurf-sails/used-sails/54235/northsails-e-type/',
-        image: 'http://www.telstarsurf.com/cache/img/788690b3da56/500/500/max/max/e-type.jpeg',
-        price: 399,
-        size: '8.2'
-      }
-    ])
+      })
+    })
+  })
+
+  describe('.getItems', function () {
+    it('get items based on the size field', function () {
+      const fixture = getFixture()
+      const items = getItems(fixture)
+
+      should(items).be.eql([
+        {
+          name: 'E-TYPE',
+          title: 'NORTHSAILS E-TYPE 7.3 €399',
+          brand: 'NORTHSAILS',
+          link: 'http://www.telstarsurf.com/windsurf/windsurf-sails/used-sails/54235/northsails-e-type/',
+          image: 'http://www.telstarsurf.com/cache/img/788690b3da56/500/500/max/max/e-type.jpeg',
+          price: 399,
+          size: '7.3'
+        },
+        {
+          name: 'E-TYPE',
+          title: 'NORTHSAILS E-TYPE 8.2 €399',
+          brand: 'NORTHSAILS',
+          link: 'http://www.telstarsurf.com/windsurf/windsurf-sails/used-sails/54235/northsails-e-type/',
+          image: 'http://www.telstarsurf.com/cache/img/788690b3da56/500/500/max/max/e-type.jpeg',
+          price: 399,
+          size: '8.2'
+        }
+      ])
+    })
+
+    it('avoid items without size', function () {
+      const fixture = getFixture().concat(getFixtureWithNoSize())
+      const items = getItems(fixture)
+
+      should(items).be.eql([
+        {
+          name: 'E-TYPE',
+          title: 'NORTHSAILS E-TYPE 7.3 €399',
+          brand: 'NORTHSAILS',
+          link: 'http://www.telstarsurf.com/windsurf/windsurf-sails/used-sails/54235/northsails-e-type/',
+          image: 'http://www.telstarsurf.com/cache/img/788690b3da56/500/500/max/max/e-type.jpeg',
+          price: 399,
+          size: '7.3'
+        },
+        {
+          name: 'E-TYPE',
+          title: 'NORTHSAILS E-TYPE 8.2 €399',
+          brand: 'NORTHSAILS',
+          link: 'http://www.telstarsurf.com/windsurf/windsurf-sails/used-sails/54235/northsails-e-type/',
+          image: 'http://www.telstarsurf.com/cache/img/788690b3da56/500/500/max/max/e-type.jpeg',
+          price: 399,
+          size: '8.2'
+        }
+      ])
+    })
   })
 })
